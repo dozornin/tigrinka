9f65df4059e61fb747e93b66babd85efd6606b64
dozornin <denioz.25@gmail.com>
2016-05-13 19:03:47 +0300

    Starting bot impl

--- tigrinka.py
+++ tigrinka.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater
+
+updater = Updater(token='147645482:AAEwfBMbjaRZq4TgyCJEbOK8o0R6KmDy1-A')
+dispatcher = updater.dispatcher
+
+def textMessageHandling(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text='Hey-ho')
+
+def photoMessageHandling(bot, update):
     pass
+
+if __name__ == "__main__":
+    text_message_handler = MessageHandler([Filters.text], textMessageHandling)
+    dispatcher.addHandler(text_message_handler)
+
+    photo_message_handler = MessageHandler([Filters.photo], photoMessageHandling)
+
+    updater.start_polling()
