4117a11d240f6c5bbe62e9467ce2f2895f37a006
dozornin <denioz.25@gmail.com>
2016-05-16 14:43:23 +0300

    Photo echo

--- tigrinka.py
+++ tigrinka.py
@@ -1,19 +1,28 @@
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import Updater
 
-updater = Updater(token='147645482:AAEwfBMbjaRZq4TgyCJEbOK8o0R6KmDy1-A')
+token = '147645482:AAEwfBMbjaRZq4TgyCJEbOK8o0R6KmDy1-A'
+updater = Updater(token=token)
 dispatcher = updater.dispatcher
+
 
 def textMessageHandling(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text='Hey-ho')
 
+
 def photoMessageHandling(bot, update):
-    pass
+    file_id = update.message.photo[-1].file_id
+    print file_id
+    print bot.getFile(file_id=file_id).file_path
+    bot.sendPhoto(chat_id=update.message.chat_id,
+                  photo=bot.getFile(file_id=file_id).file_path)
+
 
 if __name__ == "__main__":
     text_message_handler = MessageHandler([Filters.text], textMessageHandling)
-    dispatcher.addHandler(text_message_handler)
-
     photo_message_handler = MessageHandler([Filters.photo], photoMessageHandling)
+
+    dispatcher.addHandler(text_message_handler)
+    dispatcher.addHandler(photo_message_handler)
 
     updater.start_polling()
