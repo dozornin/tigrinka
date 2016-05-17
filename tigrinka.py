6c7eeff52ca85b7b3bb33cedb17e532cad9d19fe
Michael Roizner <roizner@yandex-team.ru>
2016-05-17 12:27:49 +0300

    Saving user information

--- tigrinka.py
+++ tigrinka.py
@@ -29,22 +29,20 @@ class Tigrinka(object):
         return [os.path.join('styles', filename) for filename in sorted(os.listdir('styles')) if
                 filename.endswith('.jpg')]
 
-    def get_user_status(self, chat_id):
-        # TODO: save user name
-        cursor = self._db.user_statuses.find({'chat_id': chat_id})
-        if cursor.count() == 0:
-            status = 'new'
-            self._db.user_statuses.insert_one({'chat_id': chat_id, 'status': status})
-        else:
-            assert cursor.count() == 1
-            status = cursor.next()['status']
-        return status
+    def handle_user(self, update):
+        user = update.message.from_user
+        self._db.user_info.update_one({'chat_id': update.message.chat_id},
+                                      {'$set':
+                                           {'username': user.username,
+                                            'first_name': user.first_name,
+                                            'last_name': user.last_name}},
+                                      upsert=True)
 
     def get_style(self, chat_id):
         cursor = self._db.user_styles.find({'chat_id': chat_id})
         if cursor.count() == 0:
             style = Tigrinka.RANDOM_STYLE
-            self._db.user_styles.insert_one({'chat_id': chat_id, 'style': style})
+            self._db.user_styles.update_one({'chat_id': chat_id, 'style': style}, upsert=True)
         else:
             assert cursor.count() == 1
             style = cursor.next()['style']
@@ -68,8 +66,8 @@ class Tigrinka(object):
                         'Sorry, neuro-magic is not implemented yet. Here is your original photo.')
         bot.sendPhoto(update.message.chat_id, photo=file_id)
 
-    @staticmethod
-    def show_help(bot, update):
+    def show_help(self, bot, update):
+        self.handle_user(update)
         bot.sendMessage(update.message.chat_id, '''Send me a photo and I will send you a magic in reply, or use one of these commands:
             /styles — list all available styles
             /help — show this message
@@ -97,16 +95,14 @@ class Tigrinka(object):
         updater = Updater(token=TOKEN)
         dispatcher = updater.dispatcher
 
-        # text_message_handler = MessageHandler([Filters.text], textMessageHandling)
         photo_message_handler = MessageHandler([Filters.photo], self.handle_photo_message)
-
         for style_index in xrange(len(self._styles)):
             dispatcher.add_handler(CommandHandler('style%d' % style_index, self.set_style(style_index)))
         dispatcher.add_handler(CommandHandler('stylerandom', self.set_style(-1)))
         dispatcher.add_handler(CommandHandler('styles', self.list_styles))
         dispatcher.add_handler(CommandHandler('start', self.show_help))
         dispatcher.add_handler(CommandHandler('help', self.show_help))
-        dispatcher.add_handler(MessageHandler([Filters.text], Tigrinka.show_help))
+        dispatcher.add_handler(MessageHandler([Filters.text], self.show_help))
         dispatcher.add_handler(photo_message_handler)
 
         updater.start_polling()
