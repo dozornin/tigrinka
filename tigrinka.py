d6922f189c2e5984adae0fa20035c9630c646d57
Michael Roizner <roizner@yandex-team.ru>
2016-05-18 19:34:49 +0300

    Configuring styles

--- tigrinka.py
+++ tigrinka.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
+import json
 import logging
-import os
 import random
 
+import pymongo
 from telegram.error import TelegramError
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import Updater
-
-import pymongo
 from telegram.ext.commandhandler import CommandHandler
 
 logger = logging.getLogger(__name__)
@@ -16,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 class Tigrinka(object):
     TOKEN = '147645482:AAEwfBMbjaRZq4TgyCJEbOK8o0R6KmDy1-A'
-    RANDOM_STYLE = -1
+    RANDOM_STYLE = 'random'
 
     def __init__(self):
         self._client = pymongo.MongoClient()
@@ -28,21 +27,21 @@ class Tigrinka(object):
     @staticmethod
     def read_styles():
         # TODO: consider using keys instead of indexes
-        return [os.path.join('styles', filename) for filename in sorted(os.listdir('styles')) if
-                filename.endswith('.jpg')]
+        return json.load(open('styles.json'))
 
     def send_style(self, bot, chat_id, style_index):
+        caption = self._styles[style_index]['description']
         cursor = self._db.styles.find({'style_index': style_index})
         if cursor.count() != 0:
             document = cursor.next()
             file_id = document.get('file_id')
             if file_id is not None:
                 try:
-                    bot.sendPhoto(chat_id, file_id)
+                    bot.sendPhoto(chat_id, file_id, caption=caption)
                     return
                 except TelegramError as ex:
                     logger.info('Could not locate photo %s on Telegram servers: %s', file_id, ex)
-        message = bot.sendPhoto(chat_id, open(self._styles[style_index]))
+        message = bot.sendPhoto(chat_id, open(self._styles[style_index]['filename'], 'rb'), caption=caption)
         file_id = message.photo[-1].file_id
         self._db.styles.update_one({'style_index': style_index}, {'$set': {'file_id': file_id}}, upsert=True)
 
@@ -57,12 +56,15 @@ class Tigrinka(object):
 
     def get_style(self, chat_id):
         cursor = self._db.user_styles.find({'chat_id': chat_id})
-        if cursor.count() == 0:
-            style = Tigrinka.RANDOM_STYLE
-            self._db.user_styles.update_one({'chat_id': chat_id, 'style': style}, upsert=True)
-        else:
+        style_name = Tigrinka.RANDOM_STYLE
+        if cursor.count() > 0:
             assert cursor.count() == 1
-            style = cursor.next()['style']
+            style_name = cursor.next()['style']
+        for style in self._styles:
+            if style['command'] == style_name:
+                return style
+        self._db.user_styles.update_one({'chat_id': chat_id}, {'$set': {'style': Tigrinka.RANDOM_STYLE}}, upsert=True)
+        style = self._styles[random.randint(0, len(self._styles) - 1)]
         return style
 
     def handle_photo_message(self, bot, update):
@@ -73,10 +75,8 @@ class Tigrinka(object):
         bot.getFile(file_id=file_id).download(filename)
 
         style = self.get_style(update.message.chat_id)
-        if style == Tigrinka.RANDOM_STYLE:
-            style = random.randint(0, len(self._styles) - 1)
 
-        logger.info('Should start neuro-magic on photo %s and style %s', filename, self._styles[style])
+        logger.info('Should start neuro-magic on photo %s and style %s', filename, style['command'])
         # TODO: make it async
         # TODO: implement
         bot.sendMessage(update.message.chat_id,
@@ -98,20 +98,25 @@ class Tigrinka(object):
             bot.sendMessage(chat_id, 'To use this style send me command /style%d' % style_index)
         bot.sendMessage(chat_id, 'To use random style each time send me command /stylerandom')
 
-    def set_style(self, style_index):
+    def set_style(self, style):
         def func(bot, update):
             chat_id = update.message.chat_id
-            logger.info('Setting style %d for chat %s' % (style_index, chat_id))
-            self._db.user_styles.update({'chat_id': chat_id}, {'$set': {'style': style_index}})
-            bot.sendMessage(chat_id, 'Style is set to %s' % (style_index if style_index >= 0 else 'random'))
+            description = style['description'] if style is not None else 'Random'
+            logger.info('Setting style "%s" for chat %s' % (description, chat_id))
+
+            self._db.user_styles.update({'chat_id': chat_id},
+                                        {'$set':
+                                             {'style':
+                                                  style['command'] if style is not None else Tigrinka.RANDOM_STYLE}})
+            bot.sendMessage(chat_id, 'Style is set to "%s"' % description)
 
         return func
 
     def start(self):
         dispatcher = self._updater.dispatcher
 
-        for style_index in xrange(len(self._styles)):
-            dispatcher.add_handler(CommandHandler('style%d' % style_index, self.set_style(style_index)))
+        for style in self._styles:
+            dispatcher.add_handler(CommandHandler(style['command'], self.set_style(style)))
         dispatcher.add_handler(CommandHandler('stylerandom', self.set_style(-1)))
         dispatcher.add_handler(CommandHandler('styles', self.list_styles))
         dispatcher.add_handler(CommandHandler('start', self.show_help))
@@ -120,6 +125,18 @@ class Tigrinka(object):
         dispatcher.add_handler(MessageHandler([Filters.photo], self.handle_photo_message))
 
         self._updater.start_polling()
+
+
+class ProcessTask(object):
+    def __init__(self, tigrinka, chat_id, filename, subproc):
+        self.tigrinka = tigrinka
+        self.chat_id = chat_id
+        self.filename = filename
+        self.subproc = subproc
+
+    def __call__(self, bot):
+        # TODO: implement
+        pass
 
 
 def main():
